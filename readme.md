0c125a76c7f902e64c78126cb51baa3933773399
fariscode <farizjilani@gmail.com>
2020-12-03 22:25:51 +0500

    deploying

---
+++ readme.md
@@ -0,0 +1,7 @@
+## Welcome to INK Plugin Project
+
+This project was bootstrapped with [inkplugin](https://www.npmjs.com/package/inkplugin) cli tool.
+
+### Learn More
+For more information you can refer to INK Plugin dev [documentation](https://github.com/inkcontent/dev-manual).
+
---
+++ src/main.js
@@ -0,0 +1,91 @@
+importScripts('../inkapi.js')
+
+const headingSupportTypes = ["paragraph", "heading-two", "heading-three", "heading-four"]
+
+let allBlocks = [];
+
+INKAPI.ready(() => {
+  const Editor = INKAPI.editor;
+
+  const contentChangeHandler = (data = []) => {
+    if (data.length !== 1) {
+      updateBlocks();
+      return;
+    };
+    const obj = data[0];
+    const { text, type, isBlock, path, key } = obj;
+    const prevBlockState = getBlockFromKey(key);
+    //blockquote check
+    if (
+      text.slice(0, 2).includes('> ') &&
+      headingSupportTypes.includes(type) &&
+      isBlock
+    ) {
+      if (prevBlockState) {
+        if (prevBlockState.text.slice(0, 2) !== text.slice(0, 2)) {
+          Editor.setNodeData({ type: "quote" }, path);
+        }
+      } else {
+        Editor.setNodeData({ type: "quote" }, path);
+      }
+      updateBlocks();
+      return;
+    }
+
+    //headings check
+    if (
+      text.slice(0, 5).includes('## ') &&
+      headingSupportTypes.includes(type) &&
+      isBlock
+    ) {
+      let options = null;
+      if (text.slice(0, 3) === "## ") {
+        if (prevBlockState) {
+          if (prevBlockState.text.slice(0, 3) !== text.slice(0, 3)) {
+            options = { type: "heading-two" };
+          }
+        } else {
+          options = { type: "heading-two" };
+        }
+      } else if (text.slice(0, 4) === "### ") {
+        if (prevBlockState) {
+          if (prevBlockState.text.slice(0, 4) !== text.slice(0, 4)) {
+            options = { type: "heading-three" };
+          }
+        } else {
+          options = { type: "heading-three" };
+        }
+      } else if (text.slice(0, 5) === "#### ") {
+        if (prevBlockState) {
+          if (prevBlockState.text.slice(0, 5) !== text.slice(0, 5)) {
+            options = { type: "heading-four" };
+          }
+        } else {
+          options = { type: "heading-four" };
+        }
+      }
+
+      if (options) {
+        Editor.setNodeData(options, path);
+        updateBlocks();
+        return;
+      }
+    }
+    updateBlocks();
+  };
+
+  //your code here
+  Editor.on(contentChangeHandler, "contentChange")
+
+})
+
+function getBlockFromKey(key) {
+  return allBlocks.find(b => b.key === key);
+}
+
+function updateBlocks() {
+  INKAPI.editor.getAllBlocks()
+    .then(blocks => {
+      allBlocks = blocks;
+    })
+}s
